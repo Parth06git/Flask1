c52ac2b823c11708e78aed8e526c3ae938b3c2d9
Parth06git <ce21b024@iittp.ac.in>
2022-08-27 17:44:41 +0530

    added ssl_context

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,11 @@
+from distutils.log import debug
 from app import app
 
-if __name__=="__main__":
-    app.run()
+# from OpenSSL import SSL
+
+# # context = SSL.Context(SSL.TLSv1_2_METHOD)
+# # context.use_privatekey_file("server.key")
+# # context.use_certificate_file("server.crt")
+
+if __name__ == "__main__":
+    app.run(debug=True,ssl_context="adhoc")
